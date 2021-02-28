281c8b4b556ff57921cf74ddffddcf778ed7e799
github username <github email address>
2021-02-28 13:01:31 -0500

    adding edit and delete route to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,32 @@
 class BooksController < ApplicationController
 
+    get '/book' do
+        not_logged_in?
+        @books = Book.all
+        erb :"book/index"
+    end
+
     get '/book/new' do
         not_logged_in?
         erb :"book/new"
     end
 
     post '/book' do
-        # binding.pry
         book = current_user.books.new(params)
         if book.save
-            redirect "/user/#{curent_user.id}"
+            redirect "/user/#{current_user.id}"
         else
+            @errors = book.errors.full_messages.join(" - ")
             erb :"book/new"
+        end
+    end
+
+    get '/book/:id' do
+        @book = Book.find_by(id: params[:id])
+        if @book && @book.user_id == current_user.id
+            erb :"book/show"
+        else
+            redirect "/book"
         end
     end
 
