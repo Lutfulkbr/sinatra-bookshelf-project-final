74b61a0966a6f7bdded7f344e4804394b5e94d5f
github username <github email address>
2021-03-01 08:56:28 -0500

    adding update route to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,12 +22,33 @@ class BooksController < ApplicationController
     end
 
     get '/book/:id' do
+        not_logged_in?
         @book = Book.find_by(id: params[:id])
         if @book && @book.user_id == current_user.id
             erb :"book/show"
         else
             redirect "/book"
         end
+    end
+
+    get '/book/:id/edit' do
+        not_logged_in?
+        @book = Book.find_by(id: params[:id])
+        if @book.user == current_user
+            erb :"book/edit"
+        else
+            redirect "/book/#{@book.id}"
+        end
+    end
+
+    patch '/book/:id/edit' do
+        not_logged_in?
+        @book = Book.find_by(id: params[:id])
+        if @book && @book.user_id == current_user.id
+            @book.update(params[:book])
+        end
+
+        redirect "/book/#{@book.id}"
     end
 
 end
